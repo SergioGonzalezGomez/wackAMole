e5d9d24a2c0f051b79f8bcc90d8b4f10099d72fc
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2021-12-28 18:18:33 +0100

    FeatTimer

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const hole$$ = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const mole$$ = document.querySelectorAll(".mole");
 const startButton$$ = document.querySelector("button");
-
+const timer$$ = document.querySelector('.time');
 
 let lastHole = 7;
 let score = 0;
 let timeUp;
 let temporizador;
 let lastHit = false;
+let time = 15;
 
 // Esta funcion devuelve un tiempo aleatorio en milisegundos
 function randomTime() {
-   return((Math.random()*800)+350);//pendiente de ajustar
+   return((Math.random()*800)+350);
 };
 // Esta funcion proporciona un numero de agujero diferente al que se le pasa
 function randomHole(last) {
@@ -22,7 +23,7 @@ function randomHole(last) {
     };
     return randomNumber;
 };
-
+//Esta funcion comienza el juego y llama a peep y a timer, el settimeout controla la duracion, cuando transcurra el tiempo definido timeup será true
 function startGame() {
     scoreBoard.textContent = 0;
     timeUp = false;
@@ -30,35 +31,30 @@ function startGame() {
     peep();
     setTimeout(() => {
         timeUp = true;
-        alert("Game Over!");
-        }, 15000) 
+        }, time*1000)
+    timer();
 };
 
 startButton$$.addEventListener('click', startGame);
 
-
-
 function borrarClase(param) {
     hole$$[param].classList.remove("up");
-    /* alert("se ejecuto con " + param); */
+    
 };
-
-
- function peep() {
-        let index = randomHole(lastHole);
-        hole$$[index].classList.add("up");
-        lastHit = false;
-        lastHole = index;
-        let tiempoAleatorio = randomTime();
-        /* alert("se levanta? " + index + " " + tiempoAleatorio); */
-        setTimeout(() => {
-            borrarClase(index);
-            if (timeUp == false) {
-                peep();
-            }
-        }, tiempoAleatorio); 
+//peep levanta un mole distinto al anterior y transcurrido un tiempo aleatorio esconde el mole y se llama recursivamente si timeup es false. 
+function peep() {
+    let index = randomHole(lastHole);
+    hole$$[index].classList.add("up");
+    lastHit = false;
+    lastHole = index;
+    let tiempoAleatorio = randomTime();
+    setTimeout(() => {
+        borrarClase(index);
+        if (timeUp == false) {
+            peep();
+        }
+    }, tiempoAleatorio); 
 };
-
 
 for (let i = 0; i < mole$$.length; i++){
     mole$$[i].addEventListener('click', scorePoints);
@@ -71,3 +67,15 @@ function scorePoints() {
     lastHit = true;
     };
 };
+
+function timer () {
+    setTimeout(() => {
+        timer$$.innerText = "TIME: " + time;
+        time--;
+        if (time >= 0) {
+            timer();
+        } else {
+            timer$$.innerText = "GAME OVER";
+        }  
+    },1000)
+}
